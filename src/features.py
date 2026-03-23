c3280ef0353b52180c35bad6aa42bd0d5d3cf77a
nullset-main <nullset1@proton.me>
2026-03-23 23:26:15 +0530

    final model

--- src/features.py
+++ src/features.py
@@ -7,15 +7,12 @@ import tensorflow as tf
 IMG_SIZE = (64, 64)
 
 
-def preprocess_image(img_path):
+def preprocess_image(img_path, augment=False):
     img = image.load_img(img_path, target_size=IMG_SIZE)
-    return np.array(img) / 255.0
-
-
-def preprocess_image_augmented(img_path):
-    img_array = preprocess_image(img_path)
-    img_array = tf.image.random_flip_left_right(img_array)
-    img_array = tf.image.random_brightness(img_array, max_delta=0.1)
+    img_array = np.array(img) / 255.0
+    if augment:
+        img_array = tf.image.random_flip_left_right(img_array)
+        img_array = tf.image.random_brightness(img_array, max_delta=0.1)
     return img_array
 
 
@@ -23,24 +20,18 @@ def process_folder(input_dir, output_dir, augment=False):
     os.makedirs(output_dir, exist_ok=True)
     for class_name in os.listdir(input_dir):
         class_input_path = os.path.join(input_dir, class_name)
-
+        # Skip non-directories
         if not os.path.isdir(class_input_path):
             continue
-
         class_output_path = os.path.join(output_dir, class_name)
         os.makedirs(class_output_path, exist_ok=True)
-
         for img_name in os.listdir(class_input_path):
+            # Only allow valid image extensions
             if not img_name.lower().endswith((".jpg", ".jpeg", ".png")):
                 continue
-
             img_path = os.path.join(class_input_path, img_name)
             try:
-                processed_img = (
-                    preprocess_image_augmented(img_path)
-                    if augment
-                    else preprocess_image(img_path)
-                )
+                processed_img = preprocess_image(img_path, augment)
                 save_name = img_name.rsplit(".", 1)[0] + ".npy"
                 save_path = os.path.join(class_output_path, save_name)
                 np.save(save_path, processed_img)
